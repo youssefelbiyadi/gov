4e8f17f738f2814570568c1102031d1be39a2c45
youssefelbiyadi <50877390+youssefelbiyadi@users.noreply.github.com>
2026-07-28 12:39:00 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,23 +1,6 @@
-self.ONBOARDING_STORAGE_MULTIPLIER: int = int(
-    get_env_var(name="ONBOARDING_STORAGE_MULTIPLIER", default="3")
-)
-self.ONBOARDING_USAGE_FACTOR: float = float(
-    get_env_var(name="ONBOARDING_USAGE_FACTOR", default="1.25")
-)
-self.ONBOARDING_CAPACITY_THRESHOLD: float = float(
-    get_env_var(name="ONBOARDING_CAPACITY_THRESHOLD", default="0.8")
-)
+from urllib.parse import urlparse
 
-
-
-def _evaluate_capacity(self, engine, capacity, dsource_size):
-    projected_usage = (
-        dsource_size * self.config.ONBOARDING_STORAGE_MULTIPLIER
-        + capacity.data_storage_used * self.config.ONBOARDING_USAGE_FACTOR
-    )
-    capacity_ceiling = (
-        self.config.ONBOARDING_CAPACITY_THRESHOLD * capacity.data_storage_capacity
-    )
-    if projected_usage > capacity_ceiling:
-        return None
-    return EngineOnboardingCandidate(...)
+def _get_script_name(url: str) -> str:
+    """Extract the script name from the git URL."""
+    path = urlparse(url).path
+    return path.rstrip("/").split("/")[-1]
